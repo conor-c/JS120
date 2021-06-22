0eed4a05ac59187554b3cdc4bc76a875edafdf6d
Conor Chung <ConorChung@gmail.com>
2021-06-22 16:18:26 -0400

    rps

--- rps.js
+++ rps.js
@@ -1,31 +1,3 @@
-
-// The classical approach to planning an object-oriented application
-// includes several steps:
-
-// Write a textual description of the problem or exercise.
-// Extract the significant nouns and verbs from the description.
-// Organize and associate the verbs with the nouns.
-
-
-// RPS is a two player game, where each player chooses one of three possible
-// moves, rock, paper, or scissors. The winner is selected
-// by comparing their chosen moves against each-other based on the rules.
-
-// Rules:
-// • Rock 'crushes' scissors (rock wins against scissors)
-// • Scissors 'cuts' paper (scissors wins against paper)
-// • Paper 'wraps' rock (paper wins against rock)
-// • If the moves are the same, a tie ensues
-
-// Nouns: player, move, rule
-// Verbs: choose, compare
-
-// Associate
-// Player - choose
-// Move - Rule
-
-
-// ? compare
 const readline = require('readline-sync');
 const RPSGame = {
   human: createHuman(),
@@ -39,24 +11,71 @@ const RPSGame = {
     console.log('Thanks for playing Rock, Paper, Scissors. Goodbye!');
   },
 
-  displayWinner() {
+  calculateMatchWinner() {
     let humanMove = this.human.move;
     let computerMove = this.computer.move;
-
-    console.log(`You chose: ${this.human.move}`);
-    console.log(`The computer chose: ${this.computer.move}`);
 
     if ((humanMove === 'rock' && computerMove === 'scissors') ||
         (humanMove === 'paper' && computerMove === 'rock') ||
         (humanMove === 'scissors' && computerMove === 'paper')) {
-      console.log('You Win!');
+      this.human.score += 1;
+      return 'human';
     } else if ((humanMove === 'rock' && computerMove === 'paper') ||
                (humanMove === 'paper' && computerMove === 'scissors') ||
                (humanMove === 'scissors' && computerMove === 'rock')) {
-      console.log('Computer Wins!');
-    } else {
-      console.log("It's a tie");
+      this.computer.score += 1;
+      return 'computer';
     }
+    return 'tie';
+  },
+
+  displayMatchWinner() {
+    let winner = this.calculateMatchWinner();
+
+    console.log(`You chose: ${this.human.move}`);
+    console.log(`The computer chose: ${this.computer.move}`);
+
+    switch (winner) {
+      case 'human':
+        console.log('You Win!');
+        console.log(`The score is You: ${this.human.score}, Computer: ${this.computer.score}`);
+        break;
+      case 'computer':
+        console.log('Computer Wins!');
+        console.log(`The score is You: ${this.human.score}, Computer: ${this.computer.score}`);
+        break;
+      default:
+        console.log("It's a tie.");
+        console.log(`The score is You: ${this.human.score}, Computer: ${this.computer.score}`);
+        break;
+    }
+  },
+
+  calculateRoundWinner() {
+    const MATCHES_NEEDED = 5;
+    if (this.human.score === MATCHES_NEEDED) {
+      return 'human';
+    } else if (this.computer.score === MATCHES_NEEDED) {
+      return 'computer';
+    }
+    return false;
+  },
+
+  displayRoundWinner() {
+    let winner = this.calculateRoundWinner();
+
+    switch (winner) {
+      case 'human':
+        console.log('You have won the Round!');
+        break;
+      case 'computer':
+        console.log('The computer has won the Round.');
+        break;
+      default:
+        break;
+    }
+
+    return false;
   },
 
   playAgain() {
@@ -76,8 +95,9 @@ const RPSGame = {
     while (true) {
       this.human.choose();
       this.computer.choose();
-      this.displayWinner();
-      if (!this.playAgain()) break;
+      this.displayMatchWinner();
+      this.displayRoundWinner();
+      if (this.calculateRoundWinner() && !this.playAgain()) break;
     }
 
     this.displayGoodbyeMessage();
@@ -89,6 +109,7 @@ RPSGame.play();
 function createPlayer() {
   return {
     move: null,
+    score: 0,
   };
 }
 
